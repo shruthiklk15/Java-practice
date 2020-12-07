8399c19e1c88d199533c433fc3b37c8e9b2e1b14
Shruthi <shruthip15.sk@gmail.com>
2020-12-07 23:00:51 +0530

    Program2 added tab space

--- src/program2/Program2.java
+++ src/program2/Program2.java
@@ -1,6 +1,5 @@
 class Program2 {
     public static void main(String[] args) {
-        System.out.print("Hey Girl");
-        System.out.print("how are you?");
+        System.out.print("Hey Girl\thow are you?"); //tab space
     }
 }
