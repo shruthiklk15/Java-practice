8afa71abbb502f641345c369908aaf50eaaf40fa
Shruthi <shruthip15.sk@gmail.com>
2020-12-11 09:23:45 +0530

    ascii addition

---
+++ src/program4/Program4.java
@@ -0,0 +1,8 @@
+class Program4 {
+    public static void main(String[] args){
+        System.out.println(10+10); //20
+        System.out.println("a"+"b");  //ab
+        System.out.println('a'+'b');  // ascii addition = 195
+
+    }
+}
