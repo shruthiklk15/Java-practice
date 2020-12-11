ed79232b2bbf70825976d0b1f10d57d09a8f0a87
Shruthi <shruthip15.sk@gmail.com>
2020-12-11 09:09:39 +0530

    concatination string with number

---
+++ src/program3/Program3.java
@@ -0,0 +1,9 @@
+class Program3{
+   public static void main(String[] args){
+        System.out.println("number is "+20); //Concatination
+        System.out.println(20+" is the number"); //concatination
+        System.out.println("number is "+20+20); //concatination = number is 2020
+        System.out.println(20+20+" is the number"); // addition and concatination = 40 is the number
+
+    }
+}
