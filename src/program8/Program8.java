92c9c0128eec8f45e94010b90a9379ecef5d97b3
Shruthi <shruthip15.sk@gmail.com>
2020-12-13 19:09:56 +0530

    final keyword is the const in java

---
+++ src/program8/Program8.java
@@ -0,0 +1,7 @@
+class Program8 {
+    public static void main(String[] args) {
+        final String usn =  "3sl16cs035";  //when i want to keep it constant throughout the program then I use final keyword
+        // usn = "3sl16cs040";     error: cannot assign a value to final variable usn
+        System.out.println("My constant usn throughout my engineering is " + usn);
+    }
+}
