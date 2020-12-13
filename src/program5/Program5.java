b72476865962be04d81e574349d96232e1ba4fc9
Shruthi <shruthip15.sk@gmail.com>
2020-12-13 18:03:37 +0530

    Variable declaration and initialization

---
+++ src/program5/Program5.java
@@ -0,0 +1,23 @@
+class Program5 {
+    public static void main(String[] args){
+        //  variable declration
+        int emp_id;
+        String emp_name;
+        char emp_grade;
+        double emp_salary;
+        
+        // variable initialization
+        emp_id = 1;
+        emp_name = "Shruthi";
+        emp_grade = 'A';
+        emp_salary = 20000;
+
+        // print the details
+
+        System.out.println("The employee name is\t"+ emp_name);
+        System.out.println("The employee id is  " + emp_id);
+        System.out.println("The employee grade " + emp_grade);
+        System.out.println("The employee salary is " + emp_salary);
+
+    }
+}
