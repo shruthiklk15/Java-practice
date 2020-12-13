20bfbe7fa5145fe623e0816e95dd846d28981aa1
Shruthi <shruthip15.sk@gmail.com>
2020-12-13 18:46:34 +0530

    declare and initialize for float and long

---
+++ src/program7/Program7.java
@@ -0,0 +1,9 @@
+class Program7 {
+    public static void main(String[] args) {
+        float x = 10.5f; //specify f for float
+        long z = 12345678912345l; // specify l for long
+        // float y = 10.7;  error: incompatible types: possible lossy conversion from double to float
+        System.out.println(x);
+        System.out.println(z);
+    }
+}
