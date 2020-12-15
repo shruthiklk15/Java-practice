588cd5069fe09fef718d67d3fa506b97f427aa26
Shruthi <shruthip15.sk@gmail.com>
2020-12-16 00:32:34 +0530

    conditional statements1

---
+++ src/program12/Program12.java
@@ -0,0 +1,15 @@
+class Program12{
+    public static void main(String[] args) {
+        int num = 0;
+        if(num>7){
+            System.out.println("The number is above 7");
+        }
+
+        else if(num==7){
+            System.out.println("The number is equal to 7");
+        }
+        else {
+            System.out.println("The number is below 7");
+        }
+    }
+}
