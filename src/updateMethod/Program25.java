8080031ede4a1b304daa32363f818371b2b836b9
Shruthi <shruthip15.sk@gmail.com>
2021-01-08 11:54:15 +0530

    updating variables value

---
+++ src/updateMethod/Program25.java
@@ -0,0 +1,21 @@
+class Program25
+{
+    int x = 10;
+    void update_x(int arg1)
+    {
+        System.out.println("updating x value");
+        x = arg1;
+    }
+}
+
+class MainClass25
+{
+    public static void main(String[] args)
+    {
+        Program25 object1 = new Program25();
+        System.out.println("x value before update" +object1.x);
+        object1.update_x(23);
+        System.out.println("x value after updating using method" +object1.x);
+
+    }
+}
